1b0b4fff02983c0ba97235f35c015eddfdb863b4
bassel <user.name1995@hotmail.com>
2020-12-28 21:10:38 +0200

    update RegisterController6

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,8 +68,8 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
             'office_id' => $data['office'],
+            'password' => Hash::make($data['password']),
         ]);
     }
 }
