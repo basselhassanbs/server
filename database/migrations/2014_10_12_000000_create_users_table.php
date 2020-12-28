5cc29b0fbd594035f7cb2f5037c2815da86deb63
bassel <user.name1995@hotmail.com>
2020-12-28 21:14:08 +0200

    update RegisterController7

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('office_id')->nullable();
+            $table->unsignedBigInteger('office_id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
